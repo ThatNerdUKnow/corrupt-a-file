fdb3bb14ef348ed2d81c6631800ab7e7c8d29153
withenex <brandonpi265@gmail.com>
2021-10-06 19:42:07 -0500

    Do some user friendly stuff

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const fs = require('fs')
 const buffer = require('buffer')
+const path = require('path');
+const program = require('commander')
 
-fs.copyFileSync('./input.jpg','./output.jpg')
+program
+    .version("Version? I'm never touching this code ever again lol")
+    .argument('<inputfile>','Path to the file to corrupt')
+    .option('-n, --number <number>', 'number of bytes to flip', 0)
+    .option('-d, --debug', 'enable debug output', false)
 
-function go(){
-let file = fs.readFileSync('./output.jpg')
-console.log(file)
-const offset = Math.floor(Math.random() * file.length)
-const newval = Math.floor(Math.random()*255)
-console.log(offset,newval)
-file[offset] = newval
-fs.writeFileSync('./output.jpg',file)
+program.parse(process.argv)
+
+const opts = program.opts()
+
+
+const input = program.args[0]
+const ext = path.extname(input)
+
+
+fs.copyFileSync(input, `./output${ext}`)
+
+function go() {
+    let file = fs.readFileSync(`./output${ext}`)
+    const offset = Math.floor(Math.random() * file.length)
+    const newval = Math.floor(Math.random() * 255)
+    if (opts.debug) {
+        console.log(file)
+        console.log(offset, newval)
+    }
+    file[offset] = newval
+    fs.writeFileSync(`./output${ext}`, file)
 }
 
-setInterval(()=>{go()},75)
+if (opts.number == 0) {
+    setInterval(() => { go() }, 75)
+}
+else {
+    console.log('Processing...')
+    for (let i = 0; i < program.opts().number; i++) {
+        go()
+    }
+    console.log('FINISHED')
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Corrupt-A-File
+
+## How to install/use
+
+First, you need nodejs installed then 
+```bash
+git clone https://github.com/ThatNerdUKnow/corrupt-a-file.git
+cd corrupt-a-file
+npm install
+node index.js -h
+```
